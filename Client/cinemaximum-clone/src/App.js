057dfc8dfbf9948180f63dc131cde4ea3b940b2c
bussradurmuss <bussrabam@outlook.com>
2021-10-30 23:16:48 +0300

    cinemas page header added

--- Client/cinemaximum-clone/src/App.js
+++ Client/cinemaximum-clone/src/App.js
@@ -1,9 +1,11 @@
+import Cinemas from "./components/Cinemas";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <Cinemas/>
     </div>
   );
 }
--- Client/cinemaximum-clone/src/index.js
+++ Client/cinemaximum-clone/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import "./style.css";
 import App from "./App";
 
