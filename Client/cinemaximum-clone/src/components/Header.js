abf3dc3aeb5c21336f01631877fcf778bfd5e095
Yunus Inoluk <yunusinoluk@gmail.com>
2021-10-30 23:17:52 +0300

    Main header added

--- Client/cinemaximum-clone/src/components/Header.js
+++ Client/cinemaximum-clone/src/components/Header.js
@@ -1,34 +1,71 @@
 import React, { useState } from "react";
-import "./header.css";
 import logo from "./img/cinemaximum_logo.svg";
 import goldClass from "./img/gold_class_salon_sizin-black-logo.png";
 import arthouse from "./img/arthouse-icon.png";
+import cgv from "./img/cgv_icon.svg";
+import { CgMenuLeft } from "react-icons/cg";
+import { FaSearch } from "react-icons/fa";
+import { TiTimes } from "react-icons/ti";
 
 const Header = (props) => {
+  const [isVisible, setVisible] = useState(true);
+
   return (
-    <div className="navbar">
-      <div className="container">
-        <div className="navbar__logo">
-          <img src={logo} />
+    <div className="flex h-20 items-center justify-center">
+      <div className="container flex w-full h-full items-center justify-between">
+        <div className="flex h-full items-center">
+          <img src={logo} alt="cinemaximum logo" />
+          <div className="flex items-center justify-evenly w-40 border-l-2 border-r-2 mx-8 h-full">
+            <CgMenuLeft size={30} />
+            <a href="/">Menü</a>
+          </div>
         </div>
-        <div className="navbar__menu">
-          <a href="/">Menü</a>
-        </div>
-        <div className="navbar__navigator">
-          <ul style={{ display: "flex" }}>
+
+        <div className="h-full w-[52%] px-5 flex justify-between items-center">
+          <ul className={isVisible ? "flex items-center" : "hidden"}>
             <li>
               <a href="/">Filmler</a>
             </li>
-            <li>
+            <li className="ml-10">
               <a href="/">Sinemalar</a>
             </li>
-            <li>
-              <img src={goldClass} alt="gold class" />
+            <li className="ml-10">
+              <img src={goldClass} alt="gold class" className=" h-7" />
             </li>
-            <li>
-              <img src={arthouse} alt="arthouse" />
+            <li className="ml-10">
+              <img src={arthouse} alt="arthouse" className="h-10" />
             </li>
           </ul>
+          <FaSearch
+            size={20}
+            className=""
+            onClick={() => {
+              setVisible(false);
+            }}
+          />
+
+          <input
+            placeholder="Arama"
+            className={isVisible ? "hidden" : "focus:outline-none w-[555px]"}
+          />
+          <TiTimes
+            size={25}
+            className={isVisible ? "hidden" : null}
+            onClick={() => {
+              setVisible(true);
+            }}
+          />
+        </div>
+        <div className="flex items-center border-l-2 h-full pl-8">
+          <a href="/" className="btn btn-primary mr-3">
+            Üye Ol
+          </a>
+          <a href="/" className="btn btn-light mr-3">
+            Giriş
+          </a>
+          <a href="/" className="">
+            <img src={cgv} alt=""></img>
+          </a>
         </div>
       </div>
     </div>
--- Client/cinemaximum-clone/tailwind.config.js
+++ Client/cinemaximum-clone/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#e6007e",
+        "secondary-color": "#e91756",
+        "text-color": "#000000",
+        "secondary-text-color": "#717171",
+      },
+    },
   },
   variants: {
     extend: {},
